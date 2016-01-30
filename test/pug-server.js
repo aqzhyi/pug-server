0dd76d5f560392a224dda7ca928563f8b2719dd8
pleasurazy <pleasurazy@gmail.com>
2016-01-30 23:21:13 +0800

    refactor and improve
    - missing necessary spce
    - testing should be start after server listening

--- test/pug-server.js
+++ test/pug-server.js
@@ -4,56 +4,86 @@ let expect = require('chai').expect
 let pugServer = require('../lib/pug-server')
 let fetch = require('node-fetch')
 
-describe('Hello pug-server', function() {
+describe('npm:pug-server', function() {
 
   let server
 
-  afterEach(function() {
-    server.close()
+  afterEach(function(done) {
+    server.close(done)
   })
 
-  it('Server will listening on 3000 by default', function() {
+  it('Server listening on 3000 by default', function(done) {
     server = pugServer()
-
-    return fetch('http://localhost:3000/public/index.jade')
-    .then(validateStatus)
-    .then(toText)
-    .then(function(html) {
-      expect(html).to.be.a('string')
-      expect(html).to.match(/<!DOCTYPE html>/im)
+    server.on('error', (err) => { throw err })
+    server.on('listening', () => {
+      fetch('http://localhost:3000/public/index.jade')
+      .then(validateStatus)
+      .then(toText)
+      .then(function(html) {
+        expect(html).to.be.a('string')
+        expect(html).to.match(/<!DOCTYPE html>/im)
+        done()
+      }, done)
     })
   })
 
-  it('Server can be custom views path', function() {
+  it('Server with custom views path', function(done) {
     server = pugServer('./public')
-
-    return fetch('http://localhost:3000/index.jade')
-    .then(validateStatus)
-    .then(toText)
-    .then(function(html) {
-      expect(html).to.be.a('string')
-      expect(html).to.match(/<!DOCTYPE html>/im)
+    server.on('error', (err) => { throw err })
+    server.on('listening', () => {
+      fetch('http://localhost:3000/index.jade')
+      .then(validateStatus)
+      .then(toText)
+      .then(function(html) {
+        expect(html).to.be.a('string')
+        expect(html).to.match(/<!DOCTYPE html>/im)
+        done()
+      }, done)
     })
   })
 
-  it('Server can be serve static files', function() {
+  it('Server serve static files', function(done) {
     server = pugServer('./public')
-
-    return fetch('http://localhost:3000/img/turtle.png')
-    .then(validateStatus)
-    .then(function(res) {
-      expect(res.headers.get('content-length')).to.equal('195848')
+    server.on('error', (err) => { throw err })
+    server.on('listening', () => {
+      fetch('http://localhost:3000/img/turtle.png')
+      .then(validateStatus)
+      .then(function(res) {
+        expect(res.headers.get('content-length')).to.equal('195848')
+        done()
+      }, done)
     })
   })
 
-  it('Server handled when you try request not exist files', function() {
+  it('Server response when you try request not exist file', function(done) {
     server = pugServer()
+    server.on('error', (err) => { throw err })
+    server.on('listening', () => {
+      fetch('http://localhost:3000/notExistFile.jade')
+      .then((res) => {
+        expect(res.status).to.be.at.least(400)
+        return res
+      })
+      .then(toText)
+      .then((html) => {
+        expect(html).to.match(/no such file or directory/im)
+        done()
+      }, done)
+    })
+  })
 
-    return fetch('http://localhost:3000/index.jade')
-    .then(validateStatus)
-    .then(toText)
-    .then(function(html) {
-      expect(html).to.match(/no such file or directory/im)
+  it('Server serve index.jade by default when you request http://localhost/', (done) => {
+    server = pugServer('./public')
+    server.on('error', (err) => { throw err })
+    server.on('listening', () => {
+      fetch('http://localhost:3000')
+      .then(validateStatus)
+      .then(toText)
+      .then((html) => {
+        expect(html).to.be.a('string')
+        expect(html).to.match(/<!DOCTYPE html>/im)
+        done()
+      }, done)
     })
   })
 })
@@ -67,6 +97,6 @@ function validateStatus(res) {
     return res
   }
   else {
-    throw res.status + ' ' + res.statusText
+    throw new Error(res.status + ' ' + res.statusText)
   }
 }
